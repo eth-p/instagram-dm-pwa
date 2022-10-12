c3554b751e34a4e030d78a8d4d6e7155c8b62527
Ethan P <eth-p+git@hidden.email>
2022-10-12 16:48:26 -0700

    fix(open-instagram-externally): Catch more cases

--- src/feat/open-instagram-externally.ts
+++ src/feat/open-instagram-externally.ts
@@ -8,12 +8,36 @@
 import type { ComponentProps } from "react";
 
 import type * as FastLink from "IG_FastLink";
+import type * as browserHistory from "IG_browserHistory";
 
-import { tryReexport } from "../modules";
+import { intercept, tryModules, tryReexport, wrap } from "../modules";
+
+/**
+ * Checks if a URL should open externally.
+ * @param url The URL.
+ */
+function shouldOpenExternally(url: string): boolean {
+	return url != null && !url.startsWith("/direct/");
+}
+
+/**
+ * Opens a URL if it should be opened externally.
+ * If the URL should be opened internally, will return false.
+ *
+ * @param url The URL.
+ */
+function tryOpenExternally(url: string): boolean {
+	if (shouldOpenExternally(url)) {
+		window.open(url, "_blank");
+		return true;
+	}
+
+	return false;
+}
 
 tryReexport<typeof FastLink>("FastLink", (old) => {
 	return (props: ComponentProps<typeof FastLink.default>) => {
-		if (props.href != null && !props.href.startsWith("/direct/")) {
+		if (shouldOpenExternally(props.href)) {
 			props.target = "_blank";
 		}
 
@@ -21,4 +45,33 @@ tryReexport<typeof FastLink>("FastLink", (old) => {
 			...props,
 		});
 	};
+});
+
+tryModules<[typeof browserHistory]>
+(["browserHistory"], (browserHistory) => {
+
+	intercept(browserHistory, "redirect", (thisValue, original, args) => {
+		if (!tryOpenExternally(args[0])) {
+			return original.apply(thisValue, args);
+		}
+	});
+
+	browserHistory.browserHistory.go = wrap(browserHistory.browserHistory.go, (thisValue, original, args) => {
+		if (!tryOpenExternally(args[0] /* url */)) {
+			return original.apply(thisValue, args);
+		}
+	});
+
+	browserHistory.browserHistory.push = wrap(browserHistory.browserHistory.push, (thisValue, original, args) => {
+		if (!tryOpenExternally(args[0] /* url */)) {
+			return original.apply(thisValue, args);
+		}
+	});
+
+	browserHistory.browserHistory.replace = wrap(browserHistory.browserHistory.replace, (thisValue, original, args) => {
+		if (!tryOpenExternally(args[0] /* url */)) {
+			return original.apply(thisValue, args);
+		}
+	});
+
 });
