bbc8a8429300313c194f74a50d20b8688ed65294
Ethan P <eth-p+git@hidden.email>
2022-10-13 16:51:55 -0700

    fix(remove-instagram-margins): Fix for 2022-10-13

--- src/feat/remove-instagram-margins.tsx
+++ src/feat/remove-instagram-margins.tsx
@@ -9,6 +9,7 @@ import type * as PolarisDesktopDirectPage from "IG_PolarisDesktopDirectPage.reac
 import type PolarisIGCoreBox from "IG_PolarisIGCoreBox";
 
 import { tryModules, reexport } from "../modules";
+import { visit } from "../react-util";
 
 tryModules<[typeof PolarisDesktopDirectPage, typeof PolarisIGCoreBox]>
 (["PolarisDesktopDirectPage.react", "PolarisIGCoreBox"], (PolarisDesktopDirectPage, PolarisIGCoreBox) => {
@@ -16,26 +17,20 @@ tryModules<[typeof PolarisDesktopDirectPage, typeof PolarisIGCoreBox]>
 		return (props: ComponentProps<typeof PolarisDesktopDirectPage.default>) => {
 			const root = old.default(props);
 	
-			// Find the first IGCoreBox.
-			// This will have a max width specified.
-			let child = root;
-			while (child != null && child.type !== PolarisIGCoreBox) {
-				child = child.props.children;
-			}
-	
-			// Remove the max width and border.
-			if (child != null && child.props != null) {
-				const childProps = (child.props as ComponentProps<typeof PolarisIGCoreBox>)!;
-				delete childProps.maxWidth;
-				childProps.border = false;
-			}
-	
 			// Remove the margins.
-			// root.props.className = "";
 			root.props.style = {padding: 0};
 	
-			// Return the child nodes.
-			return root;
+			// Disable the border and max width of the IGCoreBox.
+			return visit(root, (element, visit) => {
+				if (element.type === PolarisIGCoreBox) {
+					element.props.border = false;
+					delete element.props.maxWidth;
+					return element;
+				}
+
+				return visit(element);
+			});
+			
 		};
 	});
 })
