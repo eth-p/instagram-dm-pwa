01ef10dd63217c6d15802489aa5b067ea82b5f27
Ethan P <eth-p+git@hidden.email>
2022-10-14 08:48:54 -0700

    defs(PolarissetPageTitle): Fix implicit any return

--- src/ig/IG_PolarissetPageTitle.ts
+++ src/ig/IG_PolarissetPageTitle.ts
@@ -3,5 +3,5 @@ declare module "IG_PolarissetPageTitle" {
 	 * Change the page title.
 	 * @param title The new title.
 	 */
-	export default function setTitle(title: string);
+	export default function setTitle(title: string): void;
 }
